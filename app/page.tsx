fb4929d38260474f9db0924a91f630afb7741356
harsha.somarajupalli <harsha.somarajupalli@storable.com>
2023-10-12 16:27:58 +0530

    discount_ui_changes

--- app/page.tsx
+++ app/page.tsx
@@ -17,6 +17,7 @@ export default function Page() {
   const [bio, setBio] = useState("");
   const [mark, setMark] = useState<MarkType>("Email Marketing");
   const bioRef = useRef<null | HTMLDivElement>(null);
+  const [discount, setDiscount] = useState(0);
 
   const scrollToBios = () => {
     if (bioRef.current !== null) {
@@ -55,7 +56,10 @@ export default function Page() {
         scrollToBios();
       },
     });
-
+const handleDiscountChange = ()=>{
+let discount_input_ele = document.getElementById('discount_percentage_input')?.innerText;
+setDiscount(Number(discount_input_ele));
+}
   const onSubmit = (e: any) => {
     console.log("the",input)
     setBio(input);
@@ -64,9 +68,19 @@ export default function Page() {
 
   useEffect(() => {
     const ele = document.getElementById('recommended_discount_id');
+    const ele_msg = document.getElementById('discount_msg');
+    const ele_input =  document.getElementById('discount_percentage_input');
+    
     if (ele) {
-      ele.innerHTML = "Recommended Discount %:";
+      ele.innerHTML = "Recommended Discount: 12%";
     }
+    if(ele_msg) {
+      ele_msg.style.display = 'block'; 
+    }
+    // if(ele_input)
+    // {
+    //   ele_input.innerText = "12";
+    // }
   }, [mark]);
 
   // async function getRecommendedDiscount(){}
@@ -80,11 +94,30 @@ export default function Page() {
     <div className="flex mx-auto flex-col items-center justify-center py-2">
       <Header />
       <main className="flex flex-1 w-full flex-col items-center text-center px-4 mt-5 sm:mt-10">
-        <h1 className="sm:text-6xl text-4xl max-w-[708px] font-bold text-slate-900">
-          Generate your next Marketing Campaign
-        </h1>
+      <p className="text-left font-medium text-red-500">
+             Your previous month occupancy rate went down by 20%, starting a new Marketing campaign is recommended.
+            </p>
+            <br/>
+        <h3 className="sm:text-6xl text-4xl max-w-[900px] font-bold text-slate-900">
+          Generate Marketing Campaign
+        </h3>
+
         {/* <p className="text-slate-500 mt-5">47,118 bios generated so far.</p> */}
+        <br/>
         <form className="max-w-xl w-full" onSubmit={onSubmit}>
+            <p className="text-left font-medium">Discount Percentage</p>
+          <input
+            onChange={handleDiscountChange}
+            id="discount_percentage_input"
+            className="w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black my-5"
+            placeholder={
+              "please give your discount percentage"
+            }
+          />
+          <p className="text-left font-medium" id="recommended_discount_id"></p> 
+          <p className="text-left font-medium text-green-400" style={{ display: 'none' }} id="discount_msg"> This is recommended discount to maximize your revenue</p>
+          <br />
+
         
         <div className="flex mb-5 items-center space-x-3">
             {/* <Image src="/2-black.png" width={30} height={30} alt="1 icon" /> */}
@@ -111,21 +144,6 @@ export default function Page() {
           />
       
       <br />
-          <div className="flex mb-2 items-center space-x-3">
-            <p className="text-left font-medium">Discount Percentage</p>
-          </div>
-          <textarea
-            value={input}
-            onChange={handleInputChange}
-            rows={1}
-            className="w-full rounded-md border-gray-100 shadow-sm focus:border-black focus:ring-black my-5"
-            placeholder={
-              "please give your discount percentage or follow the recommendation"
-            }
-          />
-          <p className="text-left font-medium" id="recommended_discount_id"> </p>
-         
-
           {!isLoading && (
             <button
               className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-full"
